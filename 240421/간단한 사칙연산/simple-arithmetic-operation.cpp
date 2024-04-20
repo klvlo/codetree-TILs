2ae16b7dc1c621ee42f61bcda9a3dac79e06d3b3
HONG JISEUNG <81409228+klvlo@users.noreply.github.com>
2024-04-21 03:21:56 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240421/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a+b << std::endl;
+    std::cout << a-b << std::endl;
+    std::cout << a/b << std::endl;
+    std::cout << a%b;
+    return 0;
+}
