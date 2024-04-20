c3c00502666b393729bf35073c98d0c3dc385417
HONG JISEUNG <81409228+klvlo@users.noreply.github.com>
2024-04-21 03:26:28 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240421/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a << " * " << b << " = " << a*b << std::endl;
+    std::cout << a << " / " << b << " = " << a/b << std::endl;
+    return 0;
+}
